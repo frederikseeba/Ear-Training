2692d080bc2e2555c796b476aa7c2a73748feee3
frederikseeba <46448442+frederikseeba@users.noreply.github.com>
2025-03-17 10:10:54 +0000

    modified:   earTraining.py

--- earTraining.py
+++ earTraining.py
@@ -6,7 +6,7 @@ import pygame
 import streamlit as st
 import json
 
-path = ' /mount/src/ear-training/Notes/'                    # Enter your path
+path = ' /Notes/'                    # Enter your path
 
 files = [ f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) ] # Return list with filenames of notes
 files = sorted(files)
