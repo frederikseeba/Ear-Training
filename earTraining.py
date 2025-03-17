4f77cd377dc5f1113068864d1a1029bfad5e2dfc
frederikseeba <46448442+frederikseeba@users.noreply.github.com>
2025-03-17 10:30:15 +0000

    modified:   earTraining.py

--- earTraining.py
+++ earTraining.py
@@ -12,7 +12,9 @@ st.write(files)
 print(os.getcwd())
 st.write(os.getcwd())
 
-path = '/Notes/'                    # Enter your path
+path = 'Notes/'                    # Enter your path
+st.write(os.listdir('Notes/'))
+st.write(os.listdir(path))
 
 files = [ f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) ] # Return list with filenames of notes
 files = sorted(files)
